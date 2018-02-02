c84c979594d21244a600d7382e25e2f404305fe2
Caroline Lanford <caroline.lanford@gmail.com>
2018-02-02 19:28:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+let start
+let end 
+let destination
+
+function calculateVertical (){
+    
+  }
+
+function distanceFromHqInBlocks (start){
+ return Math.abs(start-42);
+}
+
+function distanceFromHqInFeet () {
+  return distanceFromHqInBlocks (start) * 264;
+
+}
+
+function distanceTravelledInFeet (){
+  calculateVertical ();
+}
+
+function calculatesFarePrice (){
+  
+}
